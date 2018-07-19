b5f8616622e83a8b997e39d7def7d3091425e609
p4gthebest <41386213+p4gthebest@users.noreply.github.com>
2018-07-19 21:09:48 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -28,4 +28,28 @@ client.user.setGame(`DragonsTeam`,"http://twitch.tv/S-F")
   console.log('')
 });
 
+
+client.on("message", message => {
+    var prefix = "["; 
+ 
+            var args = message.content.substring(prefix.length).split(" ");
+            if (message.content.startsWith(prefix + "clear")) {
+   if(!message.member.hasPermission('MANAGE_MESSAGES')) return message.reply('⚠ | **ليس لديك صلاحيات**');
+        var msg;
+        msg = parseInt();
+      
+      message.channel.fetchMessages({limit: msg}).then(messages => message.channel.bulkDelete(messages)).catch(console.error);
+      message.channel.sendMessage("", {embed: {
+        title: "Done | تــم",
+        color: 0x06DF00,
+        description: "تم مسح الرسائل بنجاح",
+        footer: {
+          text: "P4G BOT"
+        }
+      }}).then(msg => {msg.delete(3000)});
+                          }
+
+     
+});
+
 client.login(process.env.BOT_TOKEN);
