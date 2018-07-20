05a29a9aa6b8d912ac18cf975482abcde388b890
p4gthebest <41386213+p4gthebest@users.noreply.github.com>
2018-07-20 11:32:25 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -208,7 +208,8 @@ var mentionned = message.mentions.members.first();
  .setThumbnail(message.author.avatarURL)
 .addField(': تاريخ دخولك للديسكورد',` \`${moment(heg.createdTimestamp).format('YYYY/M/D HH:mm:ss')} \`**\n ${moment(heg.createdTimestamp).fromNow()}**` ,true) 
 .addField(': تاريخ دخولك لسيرفرنا', `\`${moment(h.joinedAt).format('YYYY/M/D HH:mm:ss')}  \` **\n ${moment(h.joinedAt).fromNow()} **`, true)
-
+.addfield(':عدد الدعوات',`\`${inviteCount} `)
+      
 .setFooter(message.author.username,'https://images-ext-2.discordapp.net/external/JpyzxW2wMRG2874gSTdNTpC_q9AHl8x8V4SMmtRtlVk/https/orcid.org/sites/default/files/files/ID_symbol_B-W_128x128.gif')  
     message.channel.sendEmbed(id);
 })
