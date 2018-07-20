a2580d3f88809a483270f1b781a92023d5790c26
p4gthebest <41386213+p4gthebest@users.noreply.github.com>
2018-07-20 12:22:29 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -211,8 +211,9 @@ var mentionned = message.mentions.members.first();
 .addField(` لقد قمت بدعوة :`, ` ${inviteCount} `)
       
       
-           .setFooter(`- Requested By: ${message.author.tag}`);
-           message.channel.send(mmmmEmbed)
+.setFooter(message.author.username,'https://images-ext-2.discordapp.net/external/JpyzxW2wMRG2874gSTdNTpC_q9AHl8x8V4SMmtRtlVk/https/orcid.org/sites/default/files/files/ID_symbol_B-W_128x128.gif')  
+    message.channel.sendEmbed(id);
+
 })
 }
 	   
